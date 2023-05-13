2992e53143032b00064dadec1d90b50bd4b8a5dc
aradhyasakalley <sakalleyaradhya@gmail.com>
2023-05-13 20:24:54 +0530

    AOA Experiment 1st

--- AOA/Insertion-Selection/SelectionSort.c
+++ AOA/Insertion-Selection/SelectionSort.c
@@ -21,7 +21,7 @@ void selectionSort(int arr[],int n){
 
 
 int main(){
-    int n,i,arr1[10],arr2[10],arr3[10],arr[10];
+    int n,i,arr[10000];
     clock_t start,end;
     double time_taken;
     
@@ -30,47 +30,33 @@ int main(){
     
     //for best case 
     for(i=0;i<n;i++){
-        arr1[i] = i+1;
-    }
-    start = clock();
-    selectionSort(arr1,n);
-    end = clock();
-    printf("The used array is : ");
-    for(i=0;i<n;i++){
-        printf("%d",arr1[i]);
-    }
-    printf("\n");
-    time_taken = ((double)(end - start))/CLOCKS_PER_SEC;
-    printf("The best case time is %f seconds\n",time_taken);
-
-    //for worst case 
-    for(i=0;i<n;i++){
-        arr2[i] = n-i;
+        arr[i] = i+1;
     }
     start = clock();
     selectionSort(arr,n);
     end = clock();
-    printf("The used array is : ");
+    time_taken = ((double)(end - start))/CLOCKS_PER_SEC;
+    printf("The best case time is %f seconds\n",time_taken);
+
+
+    //for worst case 
     for(i=0;i<n;i++){
-        printf("%d",arr2[i]);
+        arr[i] = n-i;
     }
-    printf("\n");
+    start = clock();
+    selectionSort(arr,n);
+    end = clock();
     time_taken = ((double)(end - start))/CLOCKS_PER_SEC;
     printf("The worst case time is %f seconds\n",time_taken);
 
 
     //for average case 
     for(i=0;i<n;i++){
-        arr3[i] = rand()%10;
+        arr[i] = rand()%10000;
     }
     start = clock();
-    selectionSort(arr3,n);
+    selectionSort(arr,n);
     end = clock();
-    printf("The used array is : ");
-    for(i=0;i<n;i++){
-        printf("%d",arr3[i]);
-    }
-    printf("\n");
     time_taken = ((double)(end - start))/CLOCKS_PER_SEC;
     printf("The average case time is %f seconds",time_taken);
 
