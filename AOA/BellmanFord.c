06e69f54780ecc58cef9cfdbe473922efb7778ff
aradhyasakalley <sakalleyaradhya@gmail.com>
2023-05-17 23:07:16 +0530

    KMP

--- AOA/BellmanFord.c
+++ AOA/BellmanFord.c
@@ -1,64 +1,64 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <limits.h>
+#include<stdio.h>
+#include<limits.h>
+#include<stdlib.h>
 
-#define V 9 // number of vertices
-#define INF INT_MAX // infinity value
-
+#define V 9
+#define INF INT_MAX
 int graph[V][V];
 
-void BellmanFord(int start) {
-    // initialize distances from start vertex to all other vertices as infinity
-    int distances[V];
-    for (int i = 0; i < V; i++) {
-        distances[i] = INF;
+void bellmanFord(int start){
+    int distance[V];
+    for(int i =0;i<V;i++){
+        distance[i] = INF;
     }
-    distances[start] = 0;
+    distance[start] = 0;
 
-    // relax edges repeatedly (V - 1) times
-    for (int i = 1; i <= V - 1; i++) {
-        for (int u = 0; u < V; u++) {
-            for (int v = 0; v < V; v++) {
+
+    for(int i =1;i<V-1;i++){
+        for(int u =0;u<V;u++){
+            for(int v = 0;v<V;v++){
                 int w = graph[u][v];
-                if (w != 0 && distances[u] != INF && distances[u] + w < distances[v]) {
-                    distances[v] = distances[u] + w;
+                if(w!=0 && distance[u]!=INF && distance[u] + w < distance[v]){
+                    distance[v] = distance[u] + w;
                 }
             }
         }
     }
 
-    // check for negative-weight cycles
-    for (int u = 0; u < V; u++) {
-        for (int v = 0; v < V; v++) {
-            int w = graph[u][v];
-            if (w != 0 && distances[u] != INF && distances[u] + w < distances[v]) {
-                printf("Graph contains a negative-weight cycle\n");
-                return;
+    //checking or negative weight cycles
+    for(int i =1;i<V-1;i++){
+        for(int u =0;u<V;u++){
+            for(int v = 0;v<V;v++){
+                int w = graph[u][v];
+                if(w!=0 && distance[u]!=INF && distance[u] + w < distance[v]){
+                   printf("Graph has negative weight cycle");
+                   return;
+                }
             }
         }
     }
 
-    // print the shortest distances from the start vertex to all other vertices
-    printf("Shortest distances from vertex %d:\n", start);
-    for (int i = 0; i < V; i++) {
-        printf("%d: %d\n", i, distances[i]);
-    }
+    //printing the distances
+    printf("The distances of all vertices from %d : \n",start);
+ 
+    for(int i =0;i<V;i++){
+        printf("%d: %d\n",i,distance[i]);
+    } 
+
+
+
 }
 
-int main() {
-    // read input
-    int num_edges;
-    scanf("%d", &num_edges);
 
-    for (int i = 0; i < num_edges; i++) {
-        int u, v, w;
-        scanf("%d %d %d", &u, &v, &w);
+int main(){
+    int num_edges,u,v,w;
+    printf("enter the number of edges : ");
+    scanf("%d",&num_edges);
+    for(int i =0;i<num_edges;i++){
+        scanf("%d %d %d",&u,&v,&w);
         graph[u][v] = w;
-        graph[v][u] = w; // add both directions for undirected graph
+        graph[v][u] = w;
     }
-
-    // run Bellman-Ford from vertex 0
-    BellmanFord(0);
-
+    bellmanFord(0);
     return 0;
 }
