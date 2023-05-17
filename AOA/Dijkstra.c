922e1c6eda5c7579393428b3d6176c3aebdcbbcf
aradhyasakalley <sakalleyaradhya@gmail.com>
2023-05-17 07:47:58 +0530

    LCS updated

--- AOA/Dijkstra.c
+++ AOA/Dijkstra.c
@@ -1,21 +1,24 @@
-#include <limits.h>
-#include <stdbool.h>
-#include <stdio.h>
+#include<stdio.h>
+#include<stdbool.h>
+#include<limits.h>
 #define V 9
+
 int minDistance(int dist[V],bool sptSet[V]){
     int min = INT_MAX,min_index;
-    for(int v = 0;v<V;v++){
-        if(sptSet[v] == false && dist[v] <= min){
-            min_index = v;
-            min = dist[v];
+    for(int i =0;i<V;i++){
+        if(sptSet[i] == false && dist[i] <= min){
+            min = dist[i];
+            min_index = i; 
         }
     }
     return min_index;
 }
+
 void printSolution(int dist[V]){
-    printf("Vertex \t\t Distance from Source\n");
-	for (int i = 0; i < V; i++)
-		printf("%d \t\t\t\t %d\n", i, dist[i]);
+    printf("Vertex\t\t\tDistace from the source\n");
+    for(int i =0;i<V;i++){
+        printf("%d\t\t\t\t%d\n",i,dist[i]);
+    }
 }
 int dijkstra(int graph[V][V],int src){
     int dist[V];
@@ -24,16 +27,15 @@ int dijkstra(int graph[V][V],int src){
         dist[i] = INT_MAX;
         sptSet[i] = false;
     }
-
     dist[src] = 0;
-    for(int j =0;j<V-1;j++){
+    for(int i =0;i<V-1;i++){
         int u = minDistance(dist,sptSet);
         sptSet[u] = true;
         for(int v =0;v<V;v++){
-            if(!sptSet[v] && graph[u][v] && dist[u]!=INT_MAX && dist[u] + graph[u][v] < dist[v]){
+            if(!sptSet[v] && graph[u][v] && dist[u]!=INT_MAX && dist[v] > dist[u] + graph[u][v]){
                 dist[v] = dist[u] + graph[u][v];
             }
-        }       
+        }
     }
     printSolution(dist);
 }
--- AOA/LCS.c
+++ AOA/LCS.c
@@ -1,16 +1,48 @@
 #include<stdio.h>
 #include<string.h>
 #define MAX 100
+
 int max(int a,int b){
     return ((a>b) ? a : b);
 }
+
+
 int LCS(char *x,char *y,int m,int n){
-    if(m == 0 || n == 0)
-       return 0;
-    else if (x[m-1] == y[n-1])
-       return 1 + LCS(x,y,m-1,n-1);
-    else 
-       return max(LCS(x,y,m-1,n),LCS(x,y,m,n-1));
+    int L[m+1][n+1];
+    for(int i = 0;i<=m;i++){
+        for(int j =0;j<=n;j++){
+            if (i == 0 || i == 0){
+                L[i][j] = 0;
+            }
+            else if(x[i-1] == y[j-1]){
+                L[i][j] = 1 + L[i-1][j-1];
+            }
+            else{
+                L[i][j] = max(L[i-1][j],L[i][j-1]);
+            }
+        }
+    }
+
+    int index = L[m][n];
+    char lcs[index+1];
+    lcs[index] = '\0';
+    int i =m,j=n;
+    while(i>0 && j>0){
+        if(x[i-1] == y[j-1]){
+            lcs[index-1] = x[i-1];
+            i--;
+            j--;
+            index--;
+        }
+        else if(L[i-1][j] > L[i][j-1]){
+            i--;
+        }
+        else{
+            j--;
+        }
+    }
+    printf("The LCS is : %s\n",lcs);
+    return L[m][n];
 }
 int main(){
     char x[MAX],y[MAX];
